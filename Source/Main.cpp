c8128bd2f968bee12d4e3a7be957e4fcfdd9dbe1
Sooah <drroessler@hotmail.com.br>
2020-10-20 16:26:46 -0300

    init_project

---
+++ Source/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// socket include
+#include <iphlpapi.h>
+#include <WS2tcpip.h>
+#include <winsock.h>
+#include <WinSock2.h>
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
