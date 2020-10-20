062c7aaa13bfd289ab80fcc9ba9aa3296bfbd9a5
Sooah <drroessler@hotmail.com.br>
2020-10-20 16:32:18 -0300

    init_2

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,3 +1,11 @@
+// Programmer: Diogo Rodrigues Roessler
+// Version product : a1.0
+// Descriptor: videochat ipv4
+// 3rdPartys: Windows Include API, OpenCV
+// Technologic used: ipv4, p2p, and others for now
+// Created: init - 20/10/2020 end - ? not yet
+//
+
 #include <iostream>
 
 // socket include
